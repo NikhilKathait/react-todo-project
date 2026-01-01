16a8e0d2fbe2e374644d0ee0b1b9ee78b24b8c2f
NIKHIL KATHAIT <nknick97@gmail.com>
2026-01-02 02:15:13 +0530

    Created react todo list project to learn and implement react components.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoRowItem from './components/TodoRowItem';
 
 function App() {
+
+  const todos = [
+    {rowNumber:1, rowDescription: 'Feed dogs', rowAssigned: 'Alice'},
+    {rowNumber:2, rowDescription: 'Learn React', rowAssigned: 'Nikhil'},
+    {rowNumber:3, rowDescription: 'Buy Groceries', rowAssigned: 'Bob'},
+    {rowNumber:4, rowDescription: 'Get Haircut', rowAssigned: 'Charlie'},
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mt-5 container'>
+      <div className='card'>
+        <div className='card-header'>
+          <h1>Todo App</h1>
+        </div>
+        <div className='card-body'>
+          <table className='table table-hover'>
+            <thead>
+              <tr>
+                <th scope ="col">#</th>
+                <th scope ="col">Description</th>
+                <th scope ="col">Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <TodoRowItem  
+                rowNumber={todos[0].rowNumber} 
+                rowDescription={todos[0].rowDescription}
+                rowAssigned={todos[0].rowAssigned} 
+              />
+              <TodoRowItem  
+                rowNumber={todos[1].rowNumber} 
+                rowDescription={todos[1].rowDescription}
+                rowAssigned={todos[1].rowAssigned} 
+              />
+              <TodoRowItem  
+                rowNumber={todos[2].rowNumber} 
+                rowDescription={todos[2].rowDescription}
+                rowAssigned={todos[2].rowAssigned} 
+              />
+              <TodoRowItem  
+                rowNumber={todos[3].rowNumber} 
+                rowDescription={todos[3].rowDescription}
+                rowAssigned={todos[3].rowAssigned} 
+              />
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
